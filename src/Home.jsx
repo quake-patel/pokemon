a4f0b0a407419b68e8496d8fefab06055a4eadf2
Quake Patel <132482378+quake-patel@users.noreply.github.com>
2024-12-15 23:14:12 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import PokemonSeasonList from "./components/PokemonSeasonList";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <PokemonSeasonList />
+    </div>
+  );
+};
+
+export default Home;
