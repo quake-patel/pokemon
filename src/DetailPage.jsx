e07fde06c440030d0d3765f5d50a8d4b36655763
Quake Patel <132482378+quake-patel@users.noreply.github.com>
2024-12-18 23:47:15 +0530

    Add files via upload

---
+++ src/DetailPage.jsx
@@ -0,0 +1,9 @@
+import { useLocation } from "react-router-dom";
+
+const location = useLocation();
+const { seasonInfo } = location.state || {};
+const DetailPage = () => {
+  return <>test</>;
+};
+
+export default DetailPage;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='container'>
+        <div className='logo'>Pokemon</div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
