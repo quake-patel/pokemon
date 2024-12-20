c5910e42acc3c5773a372189b8aba0cb17c1cf1a
Quake Patel <132482378+quake-patel@users.noreply.github.com>
2024-12-20 18:27:01 +0530

    Update DetailPage.jsx

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -1,9 +1,1 @@
-// import { useLocation } from "react-router-dom";
 
-// const location = useLocation();
-// const { seasonInfo } = location.state || {};
-const DetailPage = () => {
-  return <>test</>;
-};
-
-export default DetailPage;
